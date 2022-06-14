96ccf731e33d86476786cd25a6acf9ae786a57d0
Igor Follador <igorledf@hotmail.com>
2022-06-13 21:23:21 -0300

    Validate Token with Middleware

--- URLs.js
+++ URLs.js
@@ -1,4 +1,5 @@
 module.exports = {
     MS_CUSTOMER: 'http://localhost:3001/',
     MS_PHYSICAL_EVALUATION: 'http://localhost:3002/',
+    MS_CHECKOUT: 'http://localhost:3003/'
   };
--- index.js
+++ index.js
@@ -1,24 +1,45 @@
+require('dotenv-safe').config();
 const httpProxy = require('express-http-proxy');
 const express = require('express');
 const app = express();
 var logger = require('morgan');
+const jwt = require('jsonwebtoken');
 // const redis = require('./redisClient');
 
 app.use(logger('dev'));
 
 const {
+    MS_CHECKOUT,
     MS_CUSTOMER,
-    MS_PHYSICAL_EVALUATION,
+    MS_PHYSICAL_EVALUATION
   } = require('./URLs');
   
-  const customerServiceProxy = httpProxy(MS_CUSTOMER);
-  const physicalEvaluationServiceProxy = httpProxy(MS_PHYSICAL_EVALUATION);
-  
-  app.get('/', (req, res) => res.send('API Gateway BodyPath'));
-  
-  app.use('/customer', (req, res, next) => customerServiceProxy(req, res, next));
-  app.use('/phyisical_evaluation', (req, res, next) => physicalEvaluationServiceProxy(req, res, next));
+const checkoutServiceProxy = httpProxy(MS_CHECKOUT);
+const customerServiceProxy = httpProxy(MS_CUSTOMER);
+const physicalEvaluationServiceProxy = httpProxy(MS_PHYSICAL_EVALUATION);
 
-  app.listen(10000, () => {
-      console.log('API Gateway running!');
-  })
+app.get('/', (req, res) => res.send('API Gateway BodyPath'));
+app.use('/checkout', (req, res) => checkoutServiceProxy(req, res));
+
+app.use((req, res, next) => {
+    let token = req.headers.authorization;
+
+    if(!token) return res.sendStatus(401);
+    token = token.replace('Bearer ', "");
+        
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        console.log(token);
+        console.log(decoded.user_id);
+        if(err) return res.status(401).send({ error: 'Token inválido'});
+
+        req.user_id = decoded.user_id;
+        return next();
+    });
+})
+
+app.use('/phyisical_evaluation', (req, res, next) => physicalEvaluationServiceProxy(req, res, next));
+app.use('/customer', (req, res, next) => customerServiceProxy(req, res, next));
+
+app.listen(10000, () => {
+    console.log('API Gateway running!');
+})
