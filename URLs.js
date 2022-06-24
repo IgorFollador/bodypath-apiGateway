ed8acb17aea4837aa6dff86056759c6c0483e161
Igor Follador <igorledf@hotmail.com>
2022-06-24 15:57:50 -0300

    Update URLs to Heroku

--- URLs.js
+++ URLs.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MS_CUSTOMER: 'http://localhost:3001/',
-    MS_PHYSICAL_EVALUATION: 'http://localhost:3002/',
-    MS_CHECKOUT: 'http://localhost:3003/',
-    MS_FOOD_PLAN: 'http://localhost:3004/'
+    MS_CUSTOMER: 'https://ms-customer.herokuapp.com/',
+    MS_PHYSICAL_EVALUATION: 'https://ms-phyisical-evaluation.herokuapp.com/',
+    MS_CHECKOUT: 'https://ms-checkout.herokuapp.com/',
+    MS_FOOD_PLAN: 'https://ms-food-plan.herokuapp.com/'
   };
